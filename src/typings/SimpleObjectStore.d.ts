9475c2e609b8f48195dfccf9cca32fd45e0ce292
amitch6097 <mitch702@msu.edu>
2020-01-07 21:45:11 -0500

    update imports

--- src/typings/SimpleObjectStore.d.ts
+++ src/typings/SimpleObjectStore.d.ts
@@ -1,5 +1,5 @@
-import Store from "../Store";
-import SimpleStore from "../SimpleStore";
+import { Store } from "../Store";
+import { SimpleStore } from "../SimpleStore";
 
 export = SOSTypes;
 export as namespace SOSTypes;
